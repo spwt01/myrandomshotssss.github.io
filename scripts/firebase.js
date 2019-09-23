f55fd475e8e1ce8dcd85f67c59c5f3cba422f9af
myrandomshotssss <55654732+myrandomshotssss@users.noreply.github.com>
2019-09-23 18:50:08 +0800

    Create firebase.js

---
+++ scripts/firebase.js
@@ -0,0 +1,19 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC3LM_4OpUkxjJvNaccUxuleJ_qUmmnIN4",
+    authDomain: "myrandomshots-939f1.firebaseapp.com",
+    databaseURL: "https://myrandomshots-939f1.firebaseio.com",
+    projectId: "myrandomshots-939f1",
+    storageBucket: "",
+    messagingSenderId: "418772063596",
+    appId: "1:418772063596:web:0169598e649a0eadf28f49"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
